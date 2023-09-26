6d59d4687cb4fecd6f539fd543d4c0e76ab9c6cc
Mansigosai811 <gosaimansi811@gmail.com>
2023-09-26 14:16:51 +0530

    msg

---
+++ circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float area,radius,PI=3.14;
+	printf("enter the radius of circle");
+	scanf("%f",&radius);
+	area=PI*radius*radius;
+	printf("area is %f",area);
+
+}
