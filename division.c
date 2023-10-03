d40e38f6a3a59b12d9612817833c5a67c5e17330
Mansigosai811 <gosaimansi811@gmail.com>
2023-10-03 15:10:58 +0530

    msg

---
+++ division.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,division;
+	printf("enter the number:");
+	scanf("%d",&a);
+	printf("enter the secound number:");
+	scanf("%d",&b);
+	division=a/b;
+	printf("%d / %d = %d",a,b,division);
+}
