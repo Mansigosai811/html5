df8f098da02acbc94419dbe400b9e1154545764c
Mansigosai811 <gosaimansi811@gmail.com>
2023-11-02 13:36:19 +0530

    msg0

---
+++ marks.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int marks;
+//	printf("entyer the marks:");
+//	scanf("%d",&marks);
+//	if(marks>80)
+//	{
+//		printf("get a car");
+//	}
+//	else 
+//	{
+//		printf("get a activa");
+//	}
+//}
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("enter the marks");
+	scanf("%d",&marks);
+	if(marks>80)
+	{
+		printf("get a car");
+	}
+	else
+	{
+		printf("get a activa");
+	}
+}
