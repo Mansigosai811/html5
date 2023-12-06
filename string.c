939befa5948c39997dc0b3c7709070fa9eab91db
Mansigosai811 <gosaimansi811@gmail.com>
2023-12-06 17:37:19 +0530

    msg0

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char name[20];
+	printf("enter your name");
+	gets(name);
+	printf("name=%s",name);
+	
+	printf("\n uppercase=%s",strupr(name)); 
+	printf("\n lowercase=%s",strlwr(name));
+	printf("\n leangthcase=%d",strlen(name));
+	printf("\n revers=%s",strrev
+	
+	
+	(name));
+}
