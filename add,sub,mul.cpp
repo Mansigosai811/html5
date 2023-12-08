ff1c51f698fcf9fd9e2c80aa31b238f975043d2c
Mansigosai811 <gosaimansi811@gmail.com>
2023-12-08 15:14:15 +0530

    msg0

---
+++ add,sub,mul.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	 int a=78,b=90;
+	 cout<<"the addition of a and b:" <<a+b<<endl;
+	 cout<<"the subtrction of a and b:"<<a-b<<endl;
+	 cout<<"the multipliction of a and b:"<<a*b;
+}
