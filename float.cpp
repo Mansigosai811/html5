457d82a3aacafed14c2728fb61b29ba17b1f889e
Mansigosai811 <gosaimansi811@gmail.com>
2024-01-02 15:43:42 +0530

    msg0

---
+++ float.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout<<"enter the number : ";
+	cin>>num;
+	
+	if(num%2==0)
+	{
+		cout<<"\nthis  number is even";
+	}
+	else
+	{
+		cout<<"\nthis numbner is odd";
+	}
+}
